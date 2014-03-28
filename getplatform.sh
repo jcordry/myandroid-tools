c0c132b465b301ad79e7eacbc24fe8b966608e64
Julien Cordry <julien.cordry@gmail.com>
2014-03-28 11:01:25 +0000

    more dbug

--- getplatform.sh
+++ getplatform.sh
@@ -83,7 +83,10 @@ else
     SHA=`grep -B 1 -e "android$LEVEL" "$DEST_FOLDER/$REPXML" | grep sha1 | sed -e 's/.*sha1">//' -e 's/<.*//'`
 fi
 
-echo $FILE $SHA
+echo $FILE
+echo $SHA
+echo $LEVEL
+
 
 if [[ ! -f $FILE ]]; then
     wget "$ROOTURL/$FILE" -P "$DEST_FOLDER"
